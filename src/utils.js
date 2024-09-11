82286a8f8c28c0d0d251b4135f7e95107b3464e6
ludekarts <ludekarts@gmail.com>
2024-09-11 22:13:24 +0200

    Added usage for parseParmas util

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,19 @@
+
+/*
+  USAGE:
+
+  const { utils } = require("@ludekarts/task-runner");
+  const { parseParmas } = utils;
+  . . .
+
+  // $ node index.js --a 1 --b 2 --c
+
+  const [a, b, c] = parseParmas(process.argv.slice(2), "--a", "--b", "--c");
+
+  console.log(a, b, c); // 1 2 undefined
+
+*/
+
 function parseParmas(params, ...flags) {
   if (!Array.isArray(params)) {
     throw new Error("Cannot parse params. Params must be an array");
